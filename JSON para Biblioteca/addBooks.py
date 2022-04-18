1a7a514cb05698e57a507eb854ad22e3cbd3a561
brunoPCH <bruno.perez.ch@gmail.com>
2022-04-18 18:43:52 -0500

    Json para biblioteca utilidad para poder llenar datos

---
+++ JSON para Biblioteca/addBooks.py
@@ -0,0 +1,56 @@
+import json
+import sys
+import os
+option = "S"
+paramList=sys.argv
+def formatterArray(ObjString):
+    listFormated=ObjString.split(",")
+    return listFormated
+def makeLangObject(ObjString):
+    tempJsonObj=[]
+    listFormated=ObjString.split(",")
+    for lang in listFormated:
+        tempJsonObj.append({"name":lang})
+    return tempJsonObj
+try:
+    nameFile = paramList[1]
+    library = json.loads(open(nameFile).read())
+    while option=="S":
+        title = input("Ingrese el titulo")
+        author = formatterArray(input("Ingresa los autores separados por comas\n"))#arr
+        community=input("Ingresa las comunidades separadas por comas\n")
+        gtlog=input("Ingresa gtlog\n")
+        Gpo_lang=input("Ingresa grupo de lenguas\n")
+        year=input("Ingresa año\n")
+        source=input("Ingresa URL\n")
+        extra=input("Ingresa texto extra\n")
+        terminal_lang_es=makeLangObject(input("Ingresa las lenguas terminales en español separadas por comas\n"))#arr
+        terminal_lang_en=makeLangObject(input("Ingresa las lenguas terminales en ingles separadas por comas\n"))
+        keywords_es=formatterArray(input("Ingresa las keywords en español separadas por comas\n"))#arr
+        keywords_en=formatterArray(input("Ingresa las keywords en ingles separadas por comas\n"))#arr
+        library.append({
+            "title": title,
+            "authors": author,
+            "community":community,
+            "gtolog":gtlog,
+            "Gpo_lang":Gpo_lang,
+            "year": year,
+            "source":source,
+            "extra": extra,
+            "terminal_lang_es":terminal_lang_es ,
+            "terminal_lang_en": terminal_lang_en,
+            "keywords_es":keywords_es,
+            "keywords_en":keywords_en 
+
+        })
+        print(f'Libro agregado!!!:\n{library}')
+        option = input("deseas agregar un nuevo elemento S/N!").capitalize()
+except:
+    print(f'Error')
+
+try:
+    os.system(f'touch new{nameFile}')
+    with open(f'new{nameFile}','w') as json_file:
+        json.dump(library,json_file,ensure_ascii=False)
+except:
+    print("error al crear el archivo")
